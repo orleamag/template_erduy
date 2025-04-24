c16c1f45c09adacf5d9d03d3b8e94925d9d216e6
Orestes AG <orleamag@gmail.com>
2025-04-24 14:12:37 +0200

    translate

---
+++ src/i18n/translation.js
@@ -0,0 +1,24 @@
+export const translations = {
+    es: {
+      heroTitle: "Dr. Erduy Jesús Infante Velázquez",
+      heroTagline: "Especialista en Neurología. Especialización en Neuromuscular y Parkinson.",
+      nav: {
+        home: "Inicio",
+        publications: "Publicaciones",
+        congresses: "Congresos",
+        courses: "Cursos",
+        contact: "Contacto",
+      },
+    },
+    en: {
+      heroTitle: "Dr. Erduy Jesús Infante Velázquez",
+      heroTagline: "Neurology Specialist. Expertise in Neuromuscular and Parkinson's.",
+      nav: {
+        home: "Home",
+        publications: "Publications",
+        congresses: "Congresses",
+        courses: "Courses",
+        contact: "Contact",
+      },
+    },
+  };
